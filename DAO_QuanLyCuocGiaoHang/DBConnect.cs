2b039564d572e8dbd56ef100043e456c2d37d80c
NguyenHieuMinhTam <2424802010435@student.tdmu.edu.vn>
2026-05-14 16:12:12 +0700

    frmTheodoi

--- DAO_QuanLyCuocGiaoHang/DBConnect.cs
+++ DAO_QuanLyCuocGiaoHang/DBConnect.cs
@@ -11,27 +11,5 @@ namespace DAO_QuanLyCuocGiaoHang
 {
     public class DBConnect
     {
-        protected readonly string strConn = ConfigurationManager.ConnectionStrings["QL_CuocGiaoHang"]?.ConnectionString;
-        protected SqlConnection _conn;
-
-        public DBConnect()
-        {
-            if (string.IsNullOrEmpty(strConn))
-            {
-                throw new Exception("Chưa cấu hình chuỗi kết nối 'QL_CuocGiaoHang' trong App.config!");
-            }
-            _conn = new SqlConnection(strConn);
-        }
-        protected void OpenConnection()
-        {
-            if (_conn.State == ConnectionState.Closed)
-                _conn.Open();
-        }
-
-        protected void CloseConnection()
-        {
-            if (_conn.State == ConnectionState.Open)
-                _conn.Close();
-        }
     }
 }
