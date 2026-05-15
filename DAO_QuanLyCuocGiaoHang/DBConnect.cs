cd1e28e2b290be7187b6e1dc903473e99de115a0
TRUNG HIEU VO HO <soqua2k6@gmail.com>
2026-05-15 14:14:34 +0700

    up

---
+++ DAO_QuanLyCuocGiaoHang/DBConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO_QuanLyCuocGiaoHang
+{
+    public class DBConnect
+    {
+        protected SqlConnection _conn = new SqlConnection("");
+    }
+}
